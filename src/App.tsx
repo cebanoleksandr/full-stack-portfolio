216df720231c24aed259c93c1604da7c0ffafab6
Oleksandr Cheban <cebanoleksandr@gmail.com>
2026-08-21 16:29:24 +0300

    Implement scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Box from "@mui/material/Box";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <Box sx={{ minHeight: "100vh", backgroundColor: "background.default" }}>
+      <ScrollToTop />
       <Sidebar />
       <Box component="main" sx={{ ml: { xs: 0, md: "220px" }, width: { xs: "100%", md: "calc(100% - 220px)" } }}>
         <Outlet />
